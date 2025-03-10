5b5816813c1bebd06ebc7f525d2e4dc6f5e95580
Harish <harishvenkata65@gmail.com>
2025-03-10 16:41:56 +0530

    fetch added

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ cart.innerHTML = savedContent;
 cart.addEventListener("click", function (e) {
   e.target.remove();
 });
+
+const response = fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    if (data) {
+      let hello = document.createElement("p");
+      hello.innerText = data.title;
+      cart.appendChild(hello);
+    } else {
+      console.log(error);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
